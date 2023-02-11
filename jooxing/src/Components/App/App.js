e26e44cc27cad54cead6417db1403cc35ee01337
Punvireakroth <vireakrothpun@gmail.com>
2023-02-11 12:01:01 +0700

    initial commit ^_+

---
+++ jooxing/src/Components/App/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+// import { SearchBar } from "../SearchBar/SearchBar";
+// import { SearchResults } from "../SearchResults/SearchResults";
+// import { Playlist } from "../Playlist/Playlist";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Ja<span className="highlight">mmm</span>
+      </h1>
+      <div class="App">
+        {/* Add a SearchBar component */}
+        <div className="App-playlist">
+          {/*  Add a SearchResults component */}
+          {/*  Add a Playlist component */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
